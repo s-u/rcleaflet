406474549dee864c1f9c385f901ad81eefb135e9
Horace Ip <ipcy@research.att.com>
2015-10-13 17:41:06 -0400

    1. Use requirejs 2. Added polygon and markers R functions

--- R/rcl.R
+++ R/rcl.R
@@ -8,9 +8,9 @@ lmap <- function(lat, lon, zoom=10, where, width=800, height=600) {
     }
     if (is.null(.cache$ocaps)) {
         x <- paste(readLines(system.file("javascript", "rcl.js", package="rcleaflet"), warn=FALSE), collapse='\n')
-        l <- paste(readLines(system.file("www", "leaflet.js", package="rcleaflet"), warn=FALSE), collapse='\n')
-        x <- gsub("//LEAFLET.JS//", l, x, fixed=TRUE)
-        .cache$ocaps <- rcloud.install.js.module("rcleaflet.js", x)
+        #l <- paste(readLines(system.file("www", "leaflet.js", package="rcleaflet"), warn=FALSE), collapse='\n')
+        #x <- gsub("//LEAFLET.JS//", l, x, fixed=TRUE)
+        .cache$ocaps <- rcloud.install.js.module("rcleaflet", x, TRUE)
     }
     .cache$last.map <- structure(list(div=.cache$ocaps$map(where, as.numeric(lat), as.numeric(lon), as.integer(zoom))), class="RCloudLeaflet")
 }
@@ -18,6 +18,7 @@ lmap <- function(lat, lon, zoom=10, where, width=800, height=600) {
 ## map R colors to RGB space, also re-cycle as needed and split off RGB and A
 .mapColor <- function(col, n) {
     cc <- col2rgb(col, TRUE)
+        
     l <- list(col=substr(rgb(cc[1,], cc[2,], cc[3,], cc[4,],, 255), 1, 7), alpha=as.vector(cc[4,])/255)
     if (length(l$col) > 1 && length(l$col) != n) {
         l$col <- rep(l$col, length.out=n)
@@ -40,5 +41,32 @@ lpoints <- function(lat, lon, col="black", bg="transparent", cex=1, lwd=1, ..., 
     if (length(cex) > 1 && length(cex) != ls[1]) cex <- rep(cex, length.out=ls[1])
     if (length(lwd) > 1 && length(lwd) != ls[1]) lwd <- rep(lwd, length.out=ls[1])
     .cache$ocaps$points(map$div, lat, lon, col$col, bg$col, col$alpha, bg$alpha, cex, lwd)
+    invisible(map)
+}
+
+lmarkers <- function(lat, lon, map=.cache$last.map) {
+    if (is.null(map$div)) stop("invalid map object - not a Leaflet map")
+    ls <- c(length(lat), length(lon))
+    if (diff(ls)) { ## recycle
+        lat <- rep(lat, length.out=max(ls))
+        lon <- rep(lon, length.out=max(ls))
+    }
+    .cache$ocaps$markers(map$div, lat, lon)
+    invisible(map)
+}
+
+lpolygon <- function(lat, lon, color='red', fillColor=color,
+                     weight=5, map=.cache$last.map) {
+    if (is.null(map$div)) stop("invalid map object - not a Leaflet map")
+    ls <- c(length(lat), length(lon))
+
+    if (diff(ls)) { ## recycle
+        lat <- rep(lat, length.out=max(ls))
+        lon <- rep(lon, length.out=max(ls))
+    }
+    
+    col <- .mapColor(color, 1)
+    fill <- .mapColor(fillColor, 1)
+    .cache$ocaps$polygon(map$div, lat, lon, col$col,col$alpha, fill$col, fill$alpha,weight)
     invisible(map)
 }
--- inst/javascript/rcl.js
+++ inst/javascript/rcl.js
@@ -1,66 +1,141 @@
-({
-    map:function(div, lat, lon, zoom, k) {
-        //LEAFLET.JS//
-        var nl=document.createElement('link');
-        // FIXME: de we need this or can we just use install.css?
-        nl.setAttribute('rel', 'stylesheet');
-        nl.setAttribute('type','text/css');
-        nl.setAttribute('href','/shared.R/rcleaflet/leaflet.css');
-        document.getElementsByTagName('head')[0].appendChild(nl);
+(function(){
+    // Load CSS and JS
+    var fileref=document.createElement("link");
+    fileref.setAttribute("rel", "stylesheet");
+    fileref.setAttribute("type", "text/css");
+    fileref.setAttribute("href", 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.5/leaflet.css');
+    document.getElementsByTagName( "head" )[0].appendChild( fileref );        
+    
+    require.config({
+        paths: {
+            leaflet: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.5/leaflet'
+        },
+        shim: {
+            leaflet: {
+                exports: 'L'
+            }
+        }
+    });      
 
+    function initMap(div, lat, lon, zoom, k) {            
         $(div).resizable({stop: function() { map.invalidateSize(); }});
-
         var map = L.map($(div)[0]).setView([lat,lon], zoom);
-        L.tileLayer('http://gis.research.att.com/tiles-light/{z}/{x}/{y}.png', {maxZoom:18}).addTo(map);
-
-//        setTimeout( function() { map.updateSize();}, 200);
-
+        L.tileLayer('https://rcloud.research.att.com/tiles-light/{z}/{x}/{y}.png', {maxZoom:18}).addTo(map);
+        
         if (!window.rcleaflet) window.rcleaflet = {};
         window.rcleaflet[div] = { "L":L, "map":map };
+        
         k(null, div);
-    },
-
-    points:function(div, lat, lon, col, fill, colA, fillA, rad, lwd, k) {
-        var L = window.rcleaflet[div].L;
-        var map = window.rcleaflet[div].map;
-        if (lat.length) {
-            for (var i = 0; i < lat.length; i++)
-                L.circle([lat[i], lon[i]], rad, { color: col.charAt ? col : col[i],
-                                                  fillColor: fill.charAt ? fill : fill[i],
-                                                  fillOpacity: fillA.length ? fillA[i] : fillA,
-                                                  opacity: colA.length ? colA[i] : colA,
-                                                  weight: lwd.length ? lwd[i] : lwd,
-                                                }).addTo(map);
-        } else
-            L.circle([lat, lon], rad, { color: col, fillColor: fill, fillOpacity: fillA, opacity: colA }).addTo(map);
-        k(null, true);
-    },
-
-    segments:function(div, lat1, lon1, lat2, lon2, col, k) {
-        var L = window.rcleaflet[div].L;
-        var map = window.rcleaflet[div].map;
-        for(var i=0; i < lat1.length; i++)
-            L.polyline([[lat1[i],lon1[i]],[lat2[i],lon2[i]]], {color: col, weight:1}).addTo(map);
-        k(null, true);
-    },
-
-    polyline:function(div, lat, lon, col, weight, k) {
-        var L = window.rcleaflet[div].L;
-        var map = window.rcleaflet[div].map;
-        var points = [];
-        for(var i = 0; i < lat1.length; i++)
-            points.push(L.LatLng(lat[i], lon[i]));
-        L.polyline(points, {color: col, weight: weight}).addTo(map);
-        k(null, true);
-    },
-
-    polygon:function(div, lat, lon, col, weight, k) {
-        var L = window.rcleaflet[div].L;
-        var map = window.rcleaflet[div].map;
-        var points = [];
-        for(var i = 0; i < lat1.length; i++)
-            points.push(L.LatLng(lat[i], lon[i]));
-        L.polygon(points, {color: col, weight: weight}).addTo(map);
-        k(null, true);
     }
-})
+    
+    return {
+        map:function(div, lat, lon, zoom, k){  // this serves as an init function as well, this works because it really blocks
+            if(typeof L === 'undefined'){
+                require(['leaflet'],function(L){
+                    initMap(div, lat, lon, zoom, k);
+                });
+            }
+            else{ //no need to reload leaflet
+                initMap(div, lat, lon, zoom, k);
+            }
+        },
+        points:function(div, lat, lon, col, fill, colA, fillA, rad, lwd, k) {
+            var L = window.rcleaflet[div].L;
+            var map = window.rcleaflet[div].map;
+            if (lat.length) {
+                for (var i = 0; i < lat.length; i++)
+                    L.circle([lat[i], lon[i]], rad, { color: col.charAt ? col : col[i],
+                                                      fillColor: fill.charAt ? fill : fill[i],
+                                                      fillOpacity: fillA.length ? fillA[i] : fillA,
+                                                      opacity: colA.length ? colA[i] : colA,
+                                                      weight: lwd.length ? lwd[i] : lwd
+                                                    }).addTo(map);
+            } else
+                L.circle([lat, lon], rad, { color: col, fillColor: fill, fillOpacity: fillA, opacity: colA }).addTo(map);
+            k(null, true);
+        },
+
+        markers:function(div, lat, lon, k) {
+            var L = window.rcleaflet[div].L;
+            var map = window.rcleaflet[div].map;
+            if (lat.length) {
+                for (var i = 0; i < lat.length; i++)
+                    L.marker([lat[i], lon[i]]).addTo(map);
+            } else
+                L.marker([lat, lon]).addTo(map);
+            k(null, true);
+        },
+
+        segments:function(div, lat1, lon1, lat2, lon2, col, k) {
+            var L = window.rcleaflet[div].L;
+            var map = window.rcleaflet[div].map;
+            for(var i=0; i < lat1.length; i++)
+                L.polyline([[lat1[i],lon1[i]],[lat2[i],lon2[i]]], {color: col, weight:1}).addTo(map);
+            k(null, true);
+        },
+
+        polyline:function(div, lat, lon, col, weight, k) {
+            var L = window.rcleaflet[div].L;
+            var map = window.rcleaflet[div].map;
+            var points = [];
+            for(var i = 0; i < lat.length; i++)
+                points.push(L.latLng(lat[i], lon[i]));
+            L.polyline(points, {color: col, weight: weight}).addTo(map);
+            k(null, true);
+        },
+
+        polygon:function(div, lat, lon, color, opacity, 
+                         fillColor, fillOpacity, weight, k) {
+            var L = window.rcleaflet[div].L;
+            var map = window.rcleaflet[div].map;
+            var boundaries = [];
+            var points = [];
+
+            for(var i = 0; i < lat.length; i++){
+                if (!isNaN(lat[i])){
+                    points.push(L.latLng(lat[i], lon[i]));
+                }
+                else{
+                    var dir = points.reduce(function(p,c,i,arr){
+                        if (i >= arr.length-1){ // 
+                            return p;
+                        }
+
+                        return p + (arr[i+1].lng - arr[i].lng )*
+                            (arr[i+1].lat+arr[i].lat);
+                    },0);
+                    
+                    if (dir >= 0){ //clockwise
+                        boundaries.push([points]);
+                    }
+                    else{  //counter clockwise (hole)
+                        boundaries[boundaries.length -1].push(points);
+                    }
+                }
+            }
+            
+            //process the last set of points
+            dir = points.reduce(function(p,c,i,arr){
+                if (i >= arr.length-1){ // 
+                    return p;
+                }
+                return p + (arr[i+1].lng - arr[i].lng )*
+                    (arr[i+1].lat + arr[i].lat);
+            },0);
+            
+            if (dir >=  0){ //clockwise
+                boundaries.push([points]);
+            }
+            else{  //counter clockwise (hole)
+                boundaries[boundaries.length -1].push(points);
+            }
+            
+            L.multiPolygon(boundaries, {color: color, 
+                                        opacity: opacity,
+                                        fillColor: fillColor,
+                                        fillOpacity: fillOpacity, 
+                                        weight: weight }).addTo(map);
+            k(null, true);
+        }
+    };    
+})();
